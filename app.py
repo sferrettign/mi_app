a00477ad4cdea4f79c787084cf75534d67dbd6ee
Sebastian <sferrettign@gmail.com>
2025-05-26 21:10:22 -0400

    Primera versión

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '¡App funcionando en Render!'
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    return f'Recibí el código: {code}'
+
+if __name__ == '__main__':
+    app.run()
