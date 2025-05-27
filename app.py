99d30dc9d5e1d52ab228fe73e7a8a463bf7abb9f
Sebastian <sferrettign@gmail.com>
2025-05-27 14:27:31 -0400

    Actualizo Procfile y requirements para deploy en Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -12,4 +13,5 @@ def callback():
     return f'Recibí el código: {code}'
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
